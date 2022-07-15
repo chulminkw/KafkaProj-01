454a2794cd7563282e98c2c9561c9607e310e378
chulminkw <chulmin.kwon999@gmail.com>
2022-07-15 13:17:44 +0900

    새롭게 초기 생성

--- practice/src/main/java/com/practice/kafka/event/FileEventHandler.java
+++ practice/src/main/java/com/practice/kafka/event/FileEventHandler.java
@@ -10,19 +10,24 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
-public class FileEventHandler implements EventHandler {
 
+//모니터링하는 file에 내용이 추가되었을 때 메시지를 producer를 이용하여 전송하는 클래스
+//생성인자로 KafkaProcuer, 토픽명, SYNC 전송 여부를 전달 받음
+//EventHandler interface를 구현. onMessage() 구현.
+public class FileEventHandler implements EventHandler {
     public static final Logger logger = LoggerFactory.getLogger(FileEventHandler.class.getName());
     private KafkaProducer<String, String> kafkaProducer;
     private String topicName;
     private boolean sync;
 
+    //KafkaProducer, 토픽명, SYNC 전송여부는 생성시 입력됨.
     public FileEventHandler(KafkaProducer<String, String> kafkaProducer, String topicName, boolean sync) {
         this.kafkaProducer = kafkaProducer;
         this.topicName = topicName;
         this.sync = sync;
     }
 
+    //파일에 내용이 Append되었을 때 호출됨. 추가된 라인 별로 MessageEvent를 생성하고 이를 Producer에서 전송.
     @Override
     public void onMessage(MessageEvent messageEvent) throws InterruptedException, ExecutionException {
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>(this.topicName, messageEvent.key, messageEvent.value);
@@ -33,7 +38,7 @@ public class FileEventHandler implements EventHandler {
                     "offset:" + recordMetadata.offset() + "\n" +
                     "timestamp:" + recordMetadata.timestamp());
         } else {
-            this.kafkaProducer.send(producerRecord, (metadata, exception)-> {
+            this.kafkaProducer.send(producerRecord, (metadata, exception) -> {
                 if (exception == null) {
                     logger.info("\n ###### record metadata received ##### \n" +
                             "partition:" + metadata.partition() + "\n" +
@@ -44,8 +49,10 @@ public class FileEventHandler implements EventHandler {
                 }
             });
         }
+
     }
 
+    //FileEventHandler가 제대로 생성되었는지 확인을 위해 직접 수행.
     public static void main(String[] args) throws Exception {
         String topicName = "file-topic";
 
@@ -61,5 +68,4 @@ public class FileEventHandler implements EventHandler {
         MessageEvent messageEvent = new MessageEvent("key00001", "this is test message");
         fileEventHandler.onMessage(messageEvent);
     }
-
 }
