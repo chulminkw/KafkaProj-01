b55927825ba0b215eab4916d4eb16043c4719eb1
chulminkw <chulmin.kwon999@gmail.com>
2022-08-24 17:18:03 +0900

    최초 생성

--- producers/src/main/java/com/example/kafka/CustomPartitioner.java
+++ producers/src/main/java/com/example/kafka/CustomPartitioner.java
@@ -1,7 +1,7 @@
 package com.example.kafka;
 
 import org.apache.kafka.clients.producer.Partitioner;
-import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
+import org.apache.kafka.clients.producer.internals.StickyPartitionCache;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.InvalidRecordException;
 import org.apache.kafka.common.PartitionInfo;
@@ -9,34 +9,39 @@ import org.apache.kafka.common.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 import java.util.List;
 import java.util.Map;
 
 public class CustomPartitioner implements Partitioner {
     public static final Logger logger = LoggerFactory.getLogger(CustomPartitioner.class.getName());
+    private final StickyPartitionCache stickyPartitionCache = new StickyPartitionCache();
     private String specialKeyName;
 
     @Override
     public void configure(Map<String, ?> configs) {
-        specialKeyName = configs.get("specialKey").toString();
+        specialKeyName = configs.get("custom.specialKey").toString();
     }
 
     @Override
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
-        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
-        int numPartitions = partitions.size();
+        List<PartitionInfo> partitionInfoList = cluster.partitionsForTopic(topic);
+        int numPartitions = partitionInfoList.size();
         int numSpecialPartitions = (int)(numPartitions * 0.5);
-        int partitionIndex;
+        int partitionIndex = 0;
 
-        if ((keyBytes == null) || (!(key instanceof String)))
-            throw new InvalidRecordException("All messages must have sensor name as key");
+        if (keyBytes == null) {
+            //return stickyPartitionCache.partition(topic, cluster);
+            throw new InvalidRecordException("key should not be null");
+        }
 
-        if (((String)key).equals(this.specialKeyName))
+        if (((String)key).equals(specialKeyName)) {
             partitionIndex = Utils.toPositive(Utils.murmur2(valueBytes)) % numSpecialPartitions;
-        else
+        }
+        else {
             partitionIndex = Utils.toPositive(Utils.murmur2(keyBytes)) % (numPartitions - numSpecialPartitions) + numSpecialPartitions;
-
-        logger.info("key:{} is sent to partition:{}", key, partitionIndex);
+        }
+        logger.info("key:{} is sent to partition:{}", key.toString(), partitionIndex);
 
         return partitionIndex;
     }
